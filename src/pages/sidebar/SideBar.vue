9f2208e9ad87c1e0c919c28f29c307463aeb53a5
maoserr <maoserr@gmail.com>
2023-09-07 20:08:38 -0500

    wip

--- src/pages/sidebar/SideBar.vue
+++ src/pages/sidebar/SideBar.vue
@@ -2,22 +2,32 @@
   <div class="grid" style="width:100%">
     <div class="col-12">
       <span>{{ status_txt }}</span>
-      <TabView>
-        <TabPanel header="Chapters">
+      <Toolbar>
+        <template #end>
+          <Button label="Parse" @click="parse" class="mr-2" size="small"/>
+          <Button label="Epub" @click="load_main" icon="pi pi-book" size="small"/>
+        </template>
+      </Toolbar>
+      <Splitter layout="vertical">
+        <SplitterPanel class="flex align-items-center justify-content-center">
+          <Chaps/>
+        </SplitterPanel>
+        <SplitterPanel class="flex align-items-center justify-content-center">
+          <TabView>
+            <TabPanel header="Preview">
 
-        </TabPanel>
-        <TabPanel header="Preview">
+            </TabPanel>
+            <TabPanel header="Parsing">
 
-        </TabPanel>
-        <TabPanel header="Parsing">
-
-        </TabPanel>
-        <TabPanel header="Log">
-          <div id="log" style="overflow:auto">
-            {{ logmsgs }}
-          </div>
-        </TabPanel>
-      </TabView>
+            </TabPanel>
+            <TabPanel header="Log">
+              <div id="log" style="overflow:auto">
+                {{ logmsgs }}
+              </div>
+            </TabPanel>
+          </TabView>
+        </SplitterPanel>
+      </Splitter>
     </div>
   </div>
 </template>
@@ -25,6 +35,10 @@
 <script setup lang="ts">
 import TabPanel from "primevue/tabpanel";
 import TabView from "primevue/tabview";
+import Button from "primevue/button";
+import Toolbar from "primevue/toolbar";
+import Splitter from 'primevue/splitter';
+import SplitterPanel from 'primevue/splitterpanel';
 
 import 'primeflex/primeflex.css';
 import 'primevue/resources/primevue.min.css';
@@ -32,29 +46,31 @@ import 'primeicons/primeicons.css';
 import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
 
 import {onMounted, ref} from "vue";
+
 import ParserManager from "../../services/scraping/ParserMan";
 import MsgWindow from "../../services/messaging/MsgWindow";
 import {MsgCommand, MsgOut} from "../../services/messaging/msg_types";
+import {chaps} from "./sidebar_state"
+
+import Chaps from "./parts/Chaps.vue"
 
 const status_txt = ref<string>('Loading')
 const logmsgs = ref<string>("")
 
 const sb_origin = window.location.href
-    .split("?",2)[1]
-    .split("=",2)[1]
+    .split("?", 2)[1]
+    .split("=", 2)[1]
 const parse_man = new ParserManager(document, window)
 const msg_win = new MsgWindow(document, window,
     window.parent, sb_origin)
 
 onMounted(async () => {
   await parse_man.load_parsers()
-  console.log("Sidebar Send message")
-  const doc_info:MsgOut<{ url: string; src: string }> =
-      await msg_win.send_message<{},{url:string; src:string}>({
-    command: MsgCommand.ContGetSource,
-    data: {}
-  })
-  console.log("Sidebar Msg result", doc_info)
+  const doc_info: MsgOut<{ url: string; src: string }> =
+      await msg_win.send_message<{}, { url: string; src: string }>({
+        command: MsgCommand.ContGetSource,
+        data: {}
+      })
   const init_res = await parse_man.run_init_parser(
       {
         inputs: {},
@@ -63,6 +79,14 @@ onMounted(async () => {
       }
   )
   status_txt.value = init_res.message
+  chaps.value = init_res.data!.chaps.map(c => {
+    return {
+      info: c,
+      title: c.title,
+      html: "test",
+      html_parsed: ""
+    }
+  })
   console.log(init_res)
 })
 
--- src/pages/sidebar/parts/Chaps.vue
+++ src/pages/sidebar/parts/Chaps.vue
@@ -2,10 +2,13 @@
 
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
-import {ref} from "vue";
 
-const chaps=ref<[]>([])
-const selected_chaps=ref<[]>([])
+import {
+  chaps,
+  selected_chaps
+} from "../sidebar_state"
+
+
 </script>
 
 <template>
@@ -13,12 +16,11 @@ const selected_chaps=ref<[]>([])
              v-model:selection="selected_chaps"
              @rowReorder="chaps = $event.value"
              selectionMode="multiple"
-             scrollable scrollHeight="70vh"
+
              class="p-datatable-sm"
              responsiveLayout="scroll"
              tableStyle="min-width: 50rem">
     <Column :rowReorder="true" headerStyle="width: 3rem" :exportable="false" :reorderableColumn="false"/>
-    <Column selectionMode="multiple" style="width: 3rem" :exportable="false"></Column>
     <Column field="title" header="Title"></Column>
   </DataTable>
 </template>
